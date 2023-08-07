0f70f80c50e612636e95c968777cc57903032007
ZdorovTanya <zdorovilo.tanya@mail.ru>
2023-08-07 20:23:07 +0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Board from "./component/Board";
+import Card from "./component/Card";
 
 function App() {
   return (
     <div className="App">
-      <h1>ffff</h1>
+      <Board>
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card one</p>
+        </Card>
+      </Board>
     </div>
   );
 }
